b4ff1b9c6fbdebd63a9c5382b3fe59f95a4b31aa
Sobhan Rasoulzadeh Asl <sobhan.rasoulzade@gmail.com>
2026-02-27 13:51:26 +0330

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -80,7 +80,7 @@ async function main() {
     const has_page_repos = repos.filter(a => !a.private && a.languages?.includes("HTML"));
     console.log(`check repositorise size: ${repos.length}`);
 
-    const markdownTable = await generateMarkdownTable(has_page_repos, "https://raw.githubusercontent.com/Sobhan-SRZA/Sobhan-SRZA/4c697854a80e5e99324c04eb000f7d2cd53737ae/images/");
+    const markdownTable = await generateMarkdownTable(has_page_repos, "https://raw.githubusercontent.com/Sobhan-SRZA/Sobhan-SRZA/refs/heads/main/images/");
     fs.writeFileSync("README.md", readme(markdownTable));
     console.log("\n");
     console.log(`loaded repositorise size: ${has_page_repos.length}`);
